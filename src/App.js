0f14c4bdf7f9e3c166117e1cd67eacf9f179b97e
wesleychiau <38320889+wesleychiau@users.noreply.github.com>
2023-11-12 16:07:10 +0800

    Revert "Jenkins owasp test"
    This reverts commit 847cb8856e7cfb15e183e190976fea750a79d54b.

--- src/App.js
+++ src/App.js
@@ -6,13 +6,12 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-      <h1>TESTING</h1>
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
-          To get startedsss, edit <code>src/App.js</code> and save to reload.
+          To get started, edit <code>src/App.js</code> and save to reload.
         </p>
       </div>
     );
