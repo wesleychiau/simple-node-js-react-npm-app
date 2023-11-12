1039b86784708f76d5a0416d13ef2564aa6626a4
wesleychiau <38320889+wesleychiau@users.noreply.github.com>
2023-11-12 16:08:03 +0800

    TEST

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        <h1>TESTTTT </h1>
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
